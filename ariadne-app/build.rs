6692efed94f95570acccf08b3cab1047cb603c28
Artavazd Balaian <reasyu@gmail.com>
2025-08-10 13:17:39 +0800

    fix: switch a build pattern to Lazy for ariadne-app and ariadne-tools so it does not rebuild all the time

--- ariadne-app/build.rs
+++ ariadne-app/build.rs
@@ -2,7 +2,7 @@ use shadow_rs::{BuildPattern, ShadowBuilder};
 
 fn main() -> shadow_rs::SdResult<()> {
     let _shadow = ShadowBuilder::builder()
-        .build_pattern(BuildPattern::RealTime)
+        .build_pattern(BuildPattern::Lazy)
         .build()?;
 
     Ok(())
